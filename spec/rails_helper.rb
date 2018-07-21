7328b9f71781a3cfdf7fdfbec86b3e938c198d9f
Faizal Rakhman <faizal.rakhman@go-jek.com>
2018-07-21 14:06:26 +0700

    generate correct helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,8 +5,6 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
-#require_relative 'support/controller_admin_helpers'
-#require 'devise'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -24,20 +22,13 @@ require 'rspec/rails'
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
-# Checks for pending migration and applies them before tests are run.
+# Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
-  config.include Devise::Test::ControllerHelpers, :type => :controller
-  config.include ControllerAdminHelpers, type: :controller
-  config.include Warden::Test::Helpers
-  Warden.test_mode!
-  config.after do
-    Warden.test_reset!
-  end
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -63,7 +54,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  config.infer_spec_type_from_file_location!
-  config.include FactoryBot::Syntax::Methods
-  config.include Warden::Test::Helpers
 end
