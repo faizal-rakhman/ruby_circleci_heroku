b4da35e482abd765b793e50966b5f48851340b48
Faizal Rakhman <faizal.rakhman@go-jek.com>
2018-07-22 11:05:35 +0700

    change config

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'user/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'user#index', as: :index
+  root 'posts#index', as: :index
 end
